975fcc724c63d2d0d2ba1cf2b32e8a4083e46eb7
Jules De Smet <jules.de.smet@ll.sintgregorius.be>
2023-10-03 11:10:36 +0200

    KleurGame update
    kleur wordt willekeurig gegenereerd

--- EducatieveGame/Assets/KleurGame/Assets/Scripts/MailScript.cs
+++ EducatieveGame/Assets/KleurGame/Assets/Scripts/MailScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,12 @@ public class MailScript : MonoBehaviour
     [SerializeField] private string _color;
     [SerializeField] private string _text;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        ChooseTextAndColor();
     }
 
     // Update is called once per frame
@@ -21,8 +24,19 @@ public class MailScript : MonoBehaviour
 
     private void ChooseTextAndColor()
     {
-        Color groen = new Color(0, 1, 0);
-        //Color possibleColors[] = new Color[] {groen};
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Color groen = new Color(0, 255, 0);
+        Color rood = new Color(255, 0, 0);
+        Color blauw = new Color(0, 0, 255);
+
+        Color[] possibleColors = new Color[] { rood, groen, blauw };
+
+
+        Color chosenColor = possibleColors[UnityEngine.Random.Range(0, possibleColors.Length - 1)];
+        Debug.Log(chosenColor);
+
+        spriteRenderer.color = chosenColor;
     }
 
     public MailScript(string color, string text)
