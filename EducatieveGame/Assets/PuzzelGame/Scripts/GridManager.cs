a42506dadfe5d661dba8431e5e2ff88aa45d807a
Chair-Penetration <joeri.gerard.vermeulen@ll.sintgregorius.be>
2023-10-03 11:12:11 +0200

    Grid generatie

---
+++ EducatieveGame/Assets/PuzzelGame/Scripts/GridManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int _width, _height;
+    public Tile _tilePrefab;
+    public Transform _cam;
+    private Dictionary<Vector2, Tile> _tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateGrid()
+    {
+        _tiles = new Dictionary<Vector2, Tile>();
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0;  y < _height; y++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {IntToChar(x)} {y}";
+                var isOffset = (x + y) % 2 == 1;
+                spawnedTile.Init(isOffset);
+                _tiles[new Vector2(x, y)] = spawnedTile;
+            }
+        }
+        _cam.transform.position = new Vector3((float)_width / 2 -0.5f, (float)_height / 2 -0.5f, -10);
+    }
+
+    public Tile GetTileAtPos(Vector2 pos)
+    {
+        if (_tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private char IntToChar(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                return 'A';
+            case 1:
+                return 'B';
+            case 2:
+                return 'C';
+            case 3:
+                return 'D';
+            case 4:
+                return 'E';
+            case 5:
+                return 'F';
+            case 6:
+                return 'G';
+            case 7:
+                return 'H';
+            case 8:
+                return 'I';
+            case 9:
+                return 'J';
+            case 10:
+                return 'K';
+            case 11:
+                return 'L';
+            case 12:
+                return 'M';
+            case 13:
+                return 'N';
+            case 14:
+                return 'O';
+            case 15:
+                return 'P';
+            case 16:
+                return 'Q';
+            case 17:
+                return 'R';
+            case 18:
+                return 'S';
+            case 19:
+                return 'T';
+            case 20:
+                return 'U';
+            case 21:
+                return 'V';
+            case 22:
+                return 'W';
+            case 23:
+                return 'X';
+            case 24:
+                return 'Y';
+            case 25:
+                return 'Z';
+        }
+    }
+}
