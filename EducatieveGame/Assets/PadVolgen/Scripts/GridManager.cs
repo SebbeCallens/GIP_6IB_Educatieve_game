30836e461ed5634aa6e36677ff29249c941a1ddb
SebbeCallens <sebbe.callens@ll.sintgregorius.be>
2023-10-17 15:52:34 +0200

    grid spawner in GridManager script aangemaakt
    code van GridManager genereert nu een grid in het midden van het scherm

--- EducatieveGame/Assets/PadVolgen/Scripts/GridManager.cs
+++ EducatieveGame/Assets/PadVolgen/Scripts/GridManager.cs
@@ -6,15 +6,24 @@ public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _width, _height;
     [SerializeField] private TileScript _tilePrefab;
+    [SerializeField] private Transform _cam;
 
-    void GenerateGrid()
+    private void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
     {
         for(int x = 0; x < _width; x++)
         {
-            for(int y = 0; y < _width; y++)
+            for(int y = 0; y < _height; y++)
             {
-                
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
             }
         }
+
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
     }
 }
