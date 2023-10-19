8f6d0a5a5e36fe1779db17129d10ee46b32a902a
SebbeCallens <sebbe.callens@ll.sintgregorius.be>
2023-10-19 14:40:55 +0200

    Grid vervolledigt, highlight op grids
    grid genereert nu effectief en de tiles worden gehighlight als je met je muis over de tile gaat

--- EducatieveGame/Assets/PadVolgen/Scripts/GridManager.cs
+++ EducatieveGame/Assets/PadVolgen/Scripts/GridManager.cs
@@ -8,6 +8,8 @@ public class GridManager : MonoBehaviour
     [SerializeField] private TileScript _tilePrefab;
     [SerializeField] private Transform _cam;
 
+    private Dictionary<Vector2, TileScript> _tiles;
+
     void Start()
     {
         GenerateGrid();
@@ -15,6 +17,7 @@ public class GridManager : MonoBehaviour
 
     void GenerateGrid()
     {
+        _tiles = new Dictionary<Vector2, TileScript>();
         for(int x = 0; x < _width; x++)
         {
             for(int y = 0; y < _height; y++)
@@ -23,10 +26,22 @@ public class GridManager : MonoBehaviour
                 spawnedTile.name = $"Tile {x} {y}";
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
-                // spawnedTile.Init(isOffset);
+                spawnedTile.Init(isOffset);
+
+                _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
 
         _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+    }
+
+    public TileScript GetTileAtPosition(Vector2 pos)
+    {
+        if (_tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+
+        return null;
     }
 }
--- EducatieveGame/Assets/PadVolgen/Scripts/TileScript.cs
+++ EducatieveGame/Assets/PadVolgen/Scripts/TileScript.cs
@@ -6,16 +6,27 @@ public class TileScript : MonoBehaviour
 {
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject _highlight;
 
     public void Init(bool isOffset)
     {
         if(isOffset == true)
         {
-            _renderer.color = _baseColor;
+            _renderer.color = _offsetColor;
         }
         if(isOffset == false)
         {
             _renderer.color = _baseColor;
         }
+    }
+
+    void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        _highlight.SetActive(false);
     }
 }
