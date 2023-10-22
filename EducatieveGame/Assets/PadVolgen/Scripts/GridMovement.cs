ae3609bf50996dcac499f53e27b8f42b43b2a46c
SebbeCallens <sebbe.callens@ll.sintgregorius.be>
2023-10-23 00:17:37 +0200

    eerste versie bewegingssysteem aangemaakt
    Eerste versie van bewegingssysteem, hiermee kan de speler (binnen het grid) naar het volgende vakje springen met WASD, nog niet met klikken.

---
+++ EducatieveGame/Assets/PadVolgen/Scripts/GridMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour
+{
+    private bool _isMoving;
+    private Vector2 _origPos, _targetPos;
+    private float _timeToMove = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && !_isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector2.up));
+        }
+        if (Input.GetKey(KeyCode.A) && !_isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector2.left));
+        }
+        if (Input.GetKey(KeyCode.S) && !_isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector2.down));
+        }
+        if (Input.GetKey(KeyCode.D) && !_isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector2.right));
+        }
+    }
+
+    private IEnumerator MovePlayer(Vector2 direction)
+    {
+        _isMoving = true;
+
+        float elapsedTime = 0;
+
+        _origPos = transform.position;
+        _targetPos = _origPos + (direction);
+        if ((_targetPos.x >= 0 && _targetPos.x <= 7) && (_targetPos.y >= 0 && _targetPos.y <= 7))
+        {
+            while (elapsedTime < _timeToMove)
+            {
+                elapsedTime += Time.deltaTime;
+                transform.position = Vector3.Lerp(_origPos, _targetPos, (elapsedTime / _timeToMove));
+                yield return null;
+            }
+
+            transform.position = _targetPos;
+        }
+
+        _isMoving = false;
+    }
+}
