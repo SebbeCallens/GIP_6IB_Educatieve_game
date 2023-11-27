7c2dcae2f0dceb8bd4b8585e511c850c540fa4af
Chair-Penetration <joeri.gerard.vermeulen@ll.sintgregorius.be>
2023-11-27 11:15:11 +0100

    Better Int to Char

--- EducatieveGame/Assets/PuzzelGame/Scripts/GridManager.cs
+++ EducatieveGame/Assets/PuzzelGame/Scripts/GridManager.cs
@@ -245,7 +245,8 @@ public class GridManager2 : MonoBehaviour
 
     private char IntToChar(int num)
     {
-        switch (num)
+        return (char)('A' + num -1);
+        /**switch (num)
         {
             case 1:
                 return 'A';
@@ -300,65 +301,6 @@ public class GridManager2 : MonoBehaviour
             case 26:
                 return 'Z';
         }
-        return '!';
-    }
-    private int CharToInt(char chr)
-    {
-        switch (chr)
-        {
-            case 'A':
-                return 1;
-            case 'B':
-                return 2;
-            case 'C':
-                return 3;
-            case 'D':
-                return 4;
-            case 'E':
-                return 5;
-            case 'F':
-                return 6;
-            case 'G':
-                return 7;
-            case 'H':
-                return 8;
-            case 'I':
-                return 9;
-            case 'J':
-                return 10;
-            case 'K':
-                return 11;
-            case 'L':
-                return 12;
-            case 'M':
-                return 13;
-            case 'N':
-                return 14;
-            case 'O':
-                return 15;
-            case 'P':
-                return 16;
-            case 'Q':
-                return 17;
-            case 'R':
-                return 18;
-            case 'S':
-                return 19;
-            case 'T':
-                return 20;
-            case 'U':
-                return 21;
-            case 'V':
-                return 22;
-            case 'W':
-                return 23;
-            case 'X':
-                return 24;
-            case 'Y':
-                return 25;
-            case 'Z':
-                return 26;
-        }
-        return 0;
+        return '!';**/
     }
 }
