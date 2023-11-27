a6a8088ce33a9ed06c59b94f58d8af3222144bc6
CD-SG <cedric.dewettinck@ll.sintgregorius.be>
2023-11-27 14:12:06 +0100

    update imageslicer, voorbeeld voor implementatie
    In de scripts staat er commentaar wat alles doet + wat er nog moet gedaan worden voor volledige functionaliteit

--- EducatieveGame/Assets/PuzzelGame/Scripts/GridManagerReference.cs
+++ EducatieveGame/Assets/PuzzelGame/Scripts/GridManagerReference.cs
@@ -1,16 +1,74 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManagerReference : MonoBehaviour
 {
-    [SerializeField] private ImageSlicer _slicer;
-    [SerializeField] private Sprite _image;
+    [SerializeField] private ImageSlicer _slicer; //script dat de afbeelding snijdt
+    [SerializeField] private GameObject _imageSlot; //slot voor een stukje
+    [SerializeField] private Sprite _image; //de afbeelding
+    [SerializeField] private float _scale = 1; //schaal afbeelding
+    [SerializeField] private int _width = 4; //aantal kolommen
+    [SerializeField] private int _height = 4; //aantal rijen
 
-    public ImageSlicer Slicer { get => _slicer; set => _slicer = value; }
-    public Sprite Image { get => _image; set => _image = value; }
+    private ImageSlicer Slicer { get => _slicer; set => _slicer = value; }
+    private GameObject ImageSlot { get => _imageSlot; set => _imageSlot = value; }
+    private Sprite Image { get => _image; set => _image = value; }
+    private float Scale { get => _scale; set => _scale = value; }
+    private int Width { get => _width; set => _width = value; }
+    private int Height { get => _height; set => _height = value; }
 
     private void Awake()
     {
-        Texture2D texture = Image.texture;
-        Slicer.SliceImage(texture);
+        //hier kan je nog scale, width, height en de afbeelding ophalen
+
+        Slicer.SliceImageAdvanced(Image.texture, Scale, Width, Height); //slice afbeelding met scale, aantal kolommen en aantal rijen
+
+        List<Vector3> positions = new List<Vector3>(); //lijst van de posities van de stukjes
+        List<GameObject> parts = new List<GameObject>(); //lijst van de de stukjes
+
+        //lijsten instellen
+        for (int i = 0; i < Slicer.transform.childCount; i++)
+        {
+            positions.Add(Slicer.transform.GetChild(i).position);
+            parts.Add(Slicer.transform.GetChild(i).gameObject);
+        }
+
+        //schaal voor de slots instellen
+        float scaleX = parts[0].GetComponent<SpriteRenderer>().sprite.texture.width / 100f * parts[0].transform.localScale.x;
+        float scaleY = parts[0].GetComponent<SpriteRenderer>().sprite.texture.height / 100f * parts[0].transform.localScale.y;
+
+        //posities stukjes randomiseren
+        for (int i = positions.Count - 1; i >= 0; i--)
+        {
+            int randomIndex = Random.Range(0, parts.Count);
+            parts[randomIndex].transform.position = positions[i];
+            parts.RemoveAt(randomIndex);
+        }
+
+        //grid met slots genereren
+        //voor elke rij
+        for (int row = 0; row < Height; row++)
+        {
+            //voor elke kolom
+            for (int col = 0; col < Width; col++)
+            {
+                GameObject slot = Instantiate(ImageSlot, new(transform.position.x + scaleX * col, transform.position.y + scaleY * row), Quaternion.identity, transform);
+                slot.transform.localScale = new(scaleX, scaleY, 1);
+                slot.name = col + "-" + (Height - 1 - row);
+            }
+        }
+
+        //grid centreren
+        transform.position = new(transform.position.x - (Width / 2f * scaleX), transform.position.y - (Height / 2f * scaleY) + scaleY / 2, transform.position.z); 
     }
+
+    //doordat de naam van een slot en een afbeeldingstuk hun coordinaat zijn, kan je dit gebruiken om te controleren of het afbeeldingstuk in het juiste vakje staat
+    //je kan scripts toevoegen aan het slot en aan de stukjes om de stukjes in de slots te kunnen slepen
+    //de sprite van een slot stretched als de stukjes niet 1 op 1 zijn, hiervoor is er nog geen oplossing
+    //offsets tussen rijen en kolommen instellen in de velden van de imageslicer
+    //logica voor controle als de speler klaar is nog niet geimplementeerd
+    //om de schaal juist de berekenen zodat de afbeelding altijd mooi past op het scherm is nog niet gedaan, deze word momenteel handmatig ingesteld
+    //je zou een maximum breedte en hoogte kunnen instellen en dan via de dimensies van de afbeelding de schaal juist instellen
+    //coordinaten in A B C D, 1 2 3 4, moet nog geimplementeerd worden
+    //coordinaten waar het stukje komt moet nog boven elk stukje komen
 }
--- EducatieveGame/Assets/PuzzelGame/Scripts/ImageSlicer.cs
+++ EducatieveGame/Assets/PuzzelGame/Scripts/ImageSlicer.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class ImageSlicer : MonoBehaviour
 {
     [SerializeField] private GameObject _imagePart; //prefab voor een deel van de afbeelding waar de texture van een gesneden stuk op komt te staan
-    [SerializeField] private float _scale; //dit bepaalt de grote van de afbeelding op het scherm
-    [SerializeField] private int _width; //in hoeveel kolommen dat de afbeelding in wordt gesneden
-    [SerializeField] private int _height; //in hoeveel rijen dat de afbeelding in wordt gesneden
+    [SerializeField] private float _scale = 1; //dit bepaalt de grote van de afbeelding op het scherm
+    [SerializeField] private int _width = 4; //in hoeveel kolommen dat de afbeelding in wordt gesneden
+    [SerializeField] private int _height = 4; //in hoeveel rijen dat de afbeelding in wordt gesneden
+    [SerializeField] private float _offsetX = 0; //lege afstand tussen elke kolom
+    [SerializeField] private float _offsetY = 0; //lege afstand tussen elke rij
 
     private GameObject ImagePart { get => _imagePart; set => _imagePart = value; }
     private float Scale { get => _scale; set => _scale = value; }
     private int Width { get => _width; set => _width = value; }
     private int Height { get => _height; set => _height = value; }
+    private float OffsetX { get => _offsetX; set => _offsetX = value; }
+    private float OffsetY { get => _offsetY; set => _offsetY = value; }
 
-    public void SelectImage() //functie om een afbeelding te selecteren om te snijden
+    public void SelectImage() //functie om een afbeelding te selecteren om te snijden, deze kan je gebruiken in het menu als de speler zelf een afbeelding wilt kiezen
     {
         var extensions = new[] { new ExtensionFilter("Image Files", "png", "jpg", "jpeg") }; //hier kan je meer afbeelding extensies toevoegen als dat nodig is
 
@@ -31,7 +35,7 @@ public class ImageSlicer : MonoBehaviour
         }
     }
 
-    public void SliceImage(Texture2D image) //de afbeelding snijden
+    private void SliceImage(Texture2D image) //de afbeelding snijden
     {
         for (int i = 0; i < transform.childCount; i++) //haalt de oude stukken weg voor de nieuwe
         {
@@ -46,9 +50,9 @@ public class ImageSlicer : MonoBehaviour
         int pixelsPerUnit = 100; //pixels per unit voor de afbeelding
 
         //offset voor breedte en hoogte berekenen en daarmee de afbeelding centreren
-        float offsetX = -(Width * width * scale / pixelsPerUnit) / 2f + width * scale / pixelsPerUnit / 2f;
-        float offsetY = -(Height * height * scale / pixelsPerUnit) / 2f + height * scale / pixelsPerUnit / 2f;
-        transform.position = new Vector3(offsetX, offsetY, transform.position.z);
+        float offsetX = -(Width * width * scale / pixelsPerUnit) / 2f + width * scale / pixelsPerUnit / 2f - OffsetX * ((Width - 1) / 2f) * Scale;
+        float offsetY = -(Height * height * scale / pixelsPerUnit) / 2f + height * scale / pixelsPerUnit / 2f - OffsetY * ((Height - 1) / 2f) * Scale;
+        transform.position = new Vector3(transform.position.x + offsetX, transform.position.y + offsetY, transform.position.z);
 
         //voor elke rij
         for (int row = 0; row < Height; row++)
@@ -64,8 +68,8 @@ public class ImageSlicer : MonoBehaviour
                 texture.filterMode = FilterMode.Point;
 
                 //positie van het stuk berekenen
-                float posX = transform.position.x + col * width * scale / pixelsPerUnit;
-                float posY = transform.position.y + row * height * scale / pixelsPerUnit;
+                float posX = transform.position.x + col * width * scale / pixelsPerUnit + col * OffsetX * Scale;
+                float posY = transform.position.y + row * height * scale / pixelsPerUnit + row * OffsetY * Scale;
                 Vector3 pos = new(posX, posY, transform.position.z);
 
                 //het stuk instellen met gegeven positie
@@ -75,7 +79,16 @@ public class ImageSlicer : MonoBehaviour
                 imagePart.transform.localScale = new(scale, scale, 1);
                 Sprite sprite = Sprite.Create(texture, new Rect(0, 0, width, height), new Vector2(0.5f, 0.5f), pixelsPerUnit);
                 imagePart.GetComponent<SpriteRenderer>().sprite = sprite;
+                imagePart.name = col + "-" + (Height - 1 - row); //naam van afbeelding stuk instellen met coordinaten
             }
         }
+    }
+
+    public void SliceImageAdvanced(Texture2D texture, float scale, int width, int height) //afbeelding snijden met schaal, aantal kolommen en aantal rijen
+    {
+        Scale = scale;
+        Width = width;
+        Height = height;
+        SliceImage(texture);
     }
 }
