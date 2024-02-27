d89a7ec4ec835fad805bb99fc681b9d690c04852
CD-SG <cedric.dewettinck@ll.sintgregorius.be>
2024-02-27 09:27:25 +0100

    figuren toegevoegd

--- EducatieveGame/Assets/FigurenNamaken/Scripts/FigureListLoader.cs
+++ EducatieveGame/Assets/FigurenNamaken/Scripts/FigureListLoader.cs
@@ -37,56 +37,64 @@ public class FigureListLoader : MonoBehaviour
         if (Directory.Exists(figureDirectory))
         {
             string[] figures = Directory.GetFiles(figureDirectory, "*.txt"); //lijst met de figuren
-            List<(string figurePath, float difficulty)> figureList = new List<(string, float)>(); //lijst die voor elke figuur de moeilijkheidsgraad bijhoud
-
-            foreach (string figure in figures) //berekent moeilijkheid voor elke figuur en voegt het toe aan een lijst
+            
+            if (figures.Length > 0)
             {
-                float difficult;
+                List<(string figurePath, float difficulty)> figureList = new List<(string, float)>(); //lijst die voor elke figuur de moeilijkheidsgraad bijhoud
 
-                using (StreamReader reader = new StreamReader(figure)) //leest het figuurbestand uit
+                foreach (string figure in figures) //berekent moeilijkheid voor elke figuur en voegt het toe aan een lijst
                 {
-                    string line;
-                    float lines = -5; //ingesteld op -5 want de eerste 5 lijnen zijn info over de figuur
-                    float cellSize = 0;
+                    float difficult;
 
-                    while ((line = reader.ReadLine()) != null)
+                    using (StreamReader reader = new StreamReader(figure)) //leest het figuurbestand uit
                     {
-                        if (line.StartsWith("CellSize: "))
+                        string line;
+                        float lines = -5; //ingesteld op -5 want de eerste 5 lijnen zijn info over de figuur
+                        float cellSize = 0;
+
+                        while ((line = reader.ReadLine()) != null)
                         {
-                            string cellSizeString = line.Substring("CellSize: ".Length);
-                            cellSize = int.Parse(cellSizeString.Trim());
+                            if (line.StartsWith("CellSize: "))
+                            {
+                                string cellSizeString = line.Substring("CellSize: ".Length);
+                                cellSize = int.Parse(cellSizeString.Trim());
+                            }
+                            lines++;
                         }
-                        lines++;
+
+                        difficult = lines / cellSize / 78f; //moeilijkheid hangt af van uit hoeveel lijnen de figuur bestaat en de celgrootte
+
+                        if (difficult > 1) //moeilijkheid 1 is het maximum
+                        {
+                            difficult = 1;
+                        }
                     }
 
-                    difficult = lines / cellSize / 100; //moeilijkheid hangt af van uit hoeveel lijnen de figuur bestaat en de celgrootte
-
-                    if (difficult > 1) //moeilijkheid 1 is het maximum
-                    {
-                        difficult = 1;
-                    }
+                    figureList.Add((figure, difficult));
                 }
 
-                figureList.Add((figure, difficult));
-            }
+                figureList.Sort((a, b) => a.difficulty.CompareTo(b.difficulty)); //sorteer de figuren op moeilijkheidsgraad
 
-            figureList.Sort((a, b) => a.difficulty.CompareTo(b.difficulty)); //sorteer de figuren op moeilijkheidsgraad
-
-            int i = 0;
-            foreach ((string figurePath, float difficulty) in figureList) //leest de lijst met figuren en hun moeilijkheidsgraad en maakt voor elke figuur een knop aan
-            {
-                GameObject figureBut = Instantiate(FigureButton, transform);
-                TextMeshProUGUI[] figureText = figureBut.GetComponentsInChildren<TextMeshProUGUI>();
-                figureText[0].text = Path.GetFileNameWithoutExtension(figurePath);
-                Image[] difficultyImages = figureBut.GetComponentsInChildren<Image>();
-                int difficultyInt = Mathf.RoundToInt(difficulty * 5);
-                if (difficultyInt == 0)
+                int i = 0;
+                foreach ((string figurePath, float difficulty) in figureList) //leest de lijst met figuren en hun moeilijkheidsgraad en maakt voor elke figuur een knop aan
                 {
-                    difficultyInt = 1;
+                    GameObject figureBut = Instantiate(FigureButton, transform);
+                    TextMeshProUGUI[] figureText = figureBut.GetComponentsInChildren<TextMeshProUGUI>();
+                    figureText[0].text = Path.GetFileNameWithoutExtension(figurePath);
+                    Image[] difficultyImages = figureBut.GetComponentsInChildren<Image>();
+                    int difficultyInt = Mathf.RoundToInt(difficulty * 5);
+                    if (difficultyInt == 0)
+                    {
+                        difficultyInt = 1;
+                    }
+                    difficultyImages[1].fillAmount = difficultyInt / 5f;
+                    difficultyImages[0].color = DifficultyColor.Evaluate(difficultyInt / 5f);
+                    i++;
                 }
-                difficultyImages[1].fillAmount = difficultyInt / 5f;
-                difficultyImages[0].color = DifficultyColor.Evaluate(difficultyInt / 5f);
-                i++;
+            }
+            else
+            {
+                Instantiate(NoFigures, transform);
             }
         }
         else //als er geen figuren zijn wordt het geen figuren object geplaaatst
